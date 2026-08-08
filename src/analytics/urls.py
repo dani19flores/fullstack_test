b679af60862f09f2dd97ac0f0490d66e2dbacd89
eflores <dani19floress@gmail.com>
2026-08-07 18:58:37 -0600

    Gráfica de Ventas

--- src/analytics/urls.py
+++ src/analytics/urls.py
@@ -4,4 +4,5 @@ from analytics import views
 
 urlpatterns = [
     path("sales", views.SalesView.as_view(), name="sales-analytics"),
+    path("sales/data", views.SalesAjaxView.as_view(), name="sales-data"),
 ]
--- src/analytics/views.py
+++ src/analytics/views.py
@@ -1,9 +1,34 @@
+from datetime import timedelta
+
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.http.request import HttpRequest
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 from django.shortcuts import render
+from django.utils import timezone
 from order.models import Order
+
+class SalesAjaxView(View):
+    def get(self, request, *args, **kwargs):
+        data = {}
+        if request.user.is_staff:
+            qs = Order.objects.all().by_week_data(week_ago=5, number_of_weeks=5)
+            if request.GET.get('type') == 'week':
+                days = 7
+                start_date = timezone.now() - timedelta(days=days-1)
+                datetime_list = []
+                labels = []
+                sales_items = []
+                for x in range(0,days):
+                    new_time = start_date + timedelta(days=x)
+                    datetime_list.append(new_time)
+                    labels.append(new_time.strftime('%a'))
+                    new_qs = qs.filter(updated__day=new_time.day, updated__month=new_time.month)
+                    day_total = new_qs.total_data()['total__sum'] or 0
+                    sales_items.append(day_total)
+                data['labels'] = labels
+                data['data'] = sales_items
+        return JsonResponse(data)
 
 class SalesView(LoginRequiredMixin, TemplateView):
     template_name = 'analytics/sales.html'
