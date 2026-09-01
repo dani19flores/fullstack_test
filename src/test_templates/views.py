14d75955cfa8114c2c2aeec8aa5167775823b5ce
eflores <dani19floress@gmail.com>
2026-09-01 17:58:44 -0600

    Refactor navbar and enhance test_view template with structured layout and additional context variables

--- src/test_templates/views.py
+++ src/test_templates/views.py
@@ -3,11 +3,15 @@ import datetime
 from django.shortcuts import render
 
 def test_view(request):
+    my_list = ["Mouse", "Keyboard", "Monitor", "CPU", "GPU", "RAM", "Motherboard"]
+    empty_list = []
     context = {
         "view_title": "Test View",
         "my_number": 42,
         "my_number2": 24,
         "today": datetime.datetime.now().date(),
+        "my_list": my_list,
+        "empty_list": empty_list,
     }
     template = "test_templates/test_view.html"
     return render(request, template, context)
