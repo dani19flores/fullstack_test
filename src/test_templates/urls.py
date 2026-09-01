9fa2ab966062285c8aeab657974ff04b631bb2d1
eflores <dani19floress@gmail.com>
2026-09-01 17:44:46 -0600

    Remove admin URL path from urlpatterns and fix date retrieval in test_view context

--- src/test_templates/urls.py
+++ src/test_templates/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 
 from .views import test_view
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", test_view)
 ]
--- src/test_templates/views.py
+++ src/test_templates/views.py
@@ -7,7 +7,7 @@ def test_view(request):
         "view_title": "Test View",
         "my_number": 42,
         "my_number2": 24,
-        "today": datetime.now().todate(),
+        "today": datetime.datetime.now().date(),
     }
     template = "test_templates/test_view.html"
     return render(request, template, context)
