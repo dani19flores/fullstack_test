204da3e6f4872f82158cf1f11f5d306dc617c0eb
eflores <dani19floress@gmail.com>
2026-09-01 18:10:53 -0600

    Enhance message display in test_view template and add new base templates for improved structure

--- src/test_templates/views.py
+++ src/test_templates/views.py
@@ -1,5 +1,6 @@
-import datetime
+from datetime import datetime
 
+from django.contrib import messages
 from django.shortcuts import render
 
 def test_view(request):
@@ -9,10 +10,13 @@ def test_view(request):
         "view_title": "Test View",
         "my_number": 42,
         "my_number2": 24,
-        "today": datetime.datetime.now().date(),
+        "today": datetime.now().date(),
         "my_list": my_list,
         "empty_list": empty_list,
     }
     template = "test_templates/test_view.html"
+
+    messages.add_message(request, messages.INFO, "This is an info message.")
+    messages.add_message(request, messages.SUCCESS, "This is a success message.")
     return render(request, template, context)
 
